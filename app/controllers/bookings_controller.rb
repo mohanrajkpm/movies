aa0c70f11e17e4fe21eedc1f1271e3bdd0cf7e28
mohanrajkpm <infomohanrajsoft@gmail.com>
2020-03-01 23:59:00 +0530

    Cancel ticket

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,6 +17,12 @@ class BookingsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@booking = Booking.find(params[:id])
+		@booking.destroy
+		redirect_to bookings_path
+	end
+
 	private
 		def booking_params
     		params.require(:booking).permit(:seat_id, :screening_id).merge(user_id: current_user.id)
